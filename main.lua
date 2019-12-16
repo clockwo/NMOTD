26972ff58f85e76748db8e44036ae4f9d23e788f
Maxim <riddlexfors@gmail.com>
2019-12-16 10:47:52 +0300

    alpha verison MOTD

--- main.lua
+++ main.lua
@@ -3,17 +3,30 @@ local w, h = GetScreenWidth(), GetScreenHeight()
 
 function initframe(self)
 	self:RegisterEvent("ADDON_LOADED")
-	self:SetSize(w * 0.4, h * 0.5)
 	textedit()
 end
 
 function textedit()
-	Text_Scroll_Contenttt:SetFont('Fonts\FRIZQT___CYR.TTF', 14)
-	Text_Scroll_Contenttt:SetFontObject("p", GameFontNormal)
-	Text_Scroll_Contenttt:SetFontObject("h1", GameFontNormalHuge3)
-	Text_Scroll_Contenttt:SetFontObject("h2", GameFontNormalHuge)
-	Text_Scroll_Contenttt:SetFontObject("h3", GameFontNormalLarge)
-	Text_Scroll_Contenttt:SetText(sometext)
+	Text_Scroll_Content_One:SetFont('Fonts\FRIZQT___CYR.TTF', 14)
+	Text_Scroll_Content_One:SetFontObject("p", GameFontNormal)
+	Text_Scroll_Content_One:SetFontObject("h1", GameFontNormalHuge3)
+	Text_Scroll_Content_One:SetFontObject("h2", GameFontNormalHuge)
+	Text_Scroll_Content_One:SetFontObject("h3", GameFontNormalLarge)
+	Text_Scroll_Content_One:SetText(textone)
+
+	Text_Scroll_Content_Two:SetFont('Fonts\FRIZQT___CYR.TTF', 14)
+	Text_Scroll_Content_Two:SetFontObject("p", GameFontNormal)
+	Text_Scroll_Content_Two:SetFontObject("h1", GameFontNormalHuge3)
+	Text_Scroll_Content_Two:SetFontObject("h2", GameFontNormalHuge)
+	Text_Scroll_Content_Two:SetFontObject("h3", GameFontNormalLarge)
+	Text_Scroll_Content_Two:SetText(texttwo)
+
+	Text_Scroll_Content_Three:SetFont('Fonts\FRIZQT___CYR.TTF', 14)
+	Text_Scroll_Content_Three:SetFontObject("p", GameFontNormal)
+	Text_Scroll_Content_Three:SetFontObject("h1", GameFontNormalHuge3)
+	Text_Scroll_Content_Three:SetFontObject("h2", GameFontNormalHuge)
+	Text_Scroll_Content_Three:SetFontObject("h3", GameFontNormalLarge)
+	Text_Scroll_Content_Three:SetText(textthree)
 end
 
 --1) Левый верхний блок. Изображение, заголовок, описание
